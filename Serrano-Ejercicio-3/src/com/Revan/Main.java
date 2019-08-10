0eaa993e1a04cecc695ecda80fc43d6caae351eb
LordRevan0 <49046105+LordRevan0@users.noreply.github.com>
2019-08-10 17:54:40 -0600

    Add files via upload

---
+++ Serrano-Ejercicio-3/src/com/Revan/Main.java
@@ -0,0 +1,121 @@
+package com.Revan;
+
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Vehiculo> vehiculos = new ArrayList();
+        Scanner lector = new Scanner(System.in);
+        System.out.println("********");
+        System.out.println("Benvenido al sistema de mantenimiento");
+        System.out.println("********");
+
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("1. Ford ");
+            System.out.println("2. Toyota");
+            System.out.println("3. Honda");
+            System.out.println("4. Nissan");
+            System.out.println("5. Mostrar vehiculos");
+            System.out.println("6. Salir");
+
+
+            int opcion = lector.nextInt();
+            switch (opcion) {
+                case 1:
+                    Ford cFord = new Ford();
+                    System.out.println("Ford");
+                    System.out.print("Ingrese el color del vehiculo: ");
+                    String colorFord = lector.next();
+                    cFord.color = colorFord;
+                    System.out.print("Ingrese el año: ");
+                    int yearFord = lector.nextInt();
+                    cFord.año = yearFord;
+                    System.out.print("Ingrese el precio: ");
+                    double precioFord = lector.nextDouble();
+                    cFord.precio = precioFord;
+                    vehiculos.add(cFord);
+                    break;
+                case 2:
+                    Toyota cToyota = new Toyota();
+                    System.out.println("Toyota");
+
+                    System.out.print("Ingrese el color del vehiculo: ");
+                    String colorToyota = lector.next();
+                    cToyota.color = colorToyota;
+                    System.out.print("Ingrese el año: ");
+                    int yearToyota = lector.nextInt();
+                    cToyota.año = yearToyota;
+                    System.out.print("Ingrese el precio: ");
+                    double precioToyota = lector.nextDouble();
+                    cToyota.precio = precioToyota;
+                    vehiculos.add(cToyota);
+                    break;
+                case 3:
+                    Honda cHonda = new Honda();
+                    System.out.println("Honda");
+                    System.out.print("Ingrese el color del vehiculo: ");
+                    String colorHonda = lector.next();
+                    cHonda.color = colorHonda;
+                    System.out.print("Ingrese el año: ");
+                    int yearHonda = lector.nextInt();
+                    cHonda.año = yearHonda;
+                    System.out.print("Ingrese el precio: ");
+                    double precioHonda = lector.nextDouble();
+                    cHonda.precio = precioHonda;
+                    vehiculos.add(cHonda);
+                    break;
+                case 4:
+                    Nissan cNissan = new Nissan();
+                    System.out.println("Nissan");
+                    System.out.print("Ingrese el color del vehiculo: ");
+                    String colorNissan = lector.next();
+                    cNissan.color = colorNissan;
+                    System.out.print("Ingrese el año: ");
+                    int yearNissan = lector.nextInt();
+                    cNissan.año = yearNissan;
+                    System.out.print("Ingrese el precio: ");
+                    double precioNissan = lector.nextDouble();
+                    cNissan.precio = precioNissan;
+                    vehiculos.add(cNissan);
+
+                    break;
+                case 5:
+                    if (vehiculos.isEmpty()) {
+                        System.out.println("No hay vehiculos registrados");
+                    } else {
+                        System.out.println("Mostrando los vehiculos de Toyota y Nissan");
+                    }
+                    for (Vehiculo c : vehiculos) {
+                        String marca = "";
+                        if (c instanceof Toyota) {
+                            marca = "Toyota";
+
+                            System.out.println("el color de: " + marca + "-" + c.color);
+                            System.out.println("el año de: " + marca + "-" + c.año);
+                            System.out.println("el precio de: " + marca + "-" + c.precio);
+                        } else if (c instanceof Nissan) {
+                            marca = "Nissan";
+
+                            System.out.println("el color de: " + marca + "-" + c.color);
+                            System.out.println("el año de: " + marca + "-" + c.año);
+                            System.out.println("el precio de: " + marca + "-" + c.precio);
+                        }
+                    }
+                    break;
+                case 6:
+                    continuar = false;
+                    System.out.println("Adios");
+                    break;
+                default:
+                    System.out.println("Opción no valida:");
+                    break;
+            }
+
+
+        }
+    }
+}
---
+++ Serrano-Ejercicio-3/src/com/Revan/Vehiculo.java
@@ -0,0 +1,8 @@
+package com.Revan;
+
+public abstract class Vehiculo {
+    public String color;
+    public int año;
+    public double precio;
+
+}
